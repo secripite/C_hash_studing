bbbba942a04f609cab89c48dd6eecb0f7e6853d4
secripite <keonho04@gmail.com>
2023-10-05 15:36:22 +0900

    commit

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+﻿Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
 
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
